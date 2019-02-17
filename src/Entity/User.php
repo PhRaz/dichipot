2e3dd68c48397447e59a67f88143144d58d819cc
PhRaz <philippe.razavet@gmail.com>
2019-02-17 20:49:42 +0100

    ajout User / Event / UserEvent

---
+++ src/Entity/User.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\UserEvent", mappedBy="user")
+     */
+    private $UserEvents;
+
+    public function __construct()
+    {
+        $this->UserEvents = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|UserEvent[]
+     */
+    public function getUserEvents(): Collection
+    {
+        return $this->UserEvents;
+    }
+
+    public function addUserEvent(UserEvent $userEvent): self
+    {
+        if (!$this->UserEvents->contains($userEvent)) {
+            $this->UserEvents[] = $userEvent;
+            $userEvent->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserEvent(UserEvent $userEvent): self
+    {
+        if ($this->UserEvents->contains($userEvent)) {
+            $this->UserEvents->removeElement($userEvent);
+            // set the owning side to null (unless already changed)
+            if ($userEvent->getUser() === $this) {
+                $userEvent->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+}
