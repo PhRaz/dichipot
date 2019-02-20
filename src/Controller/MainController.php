e61bc8607238938d90d6a5578d9e8a01e8fb4a9f
PhRaz <philippe.razavet@gmail.com>
2019-02-20 22:16:32 +0100

    wip

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,13 +11,40 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home() {
+        return $this->render("home.html.twig");
+    }
+
+    /**
+     * @Route("/user/list", name="user_list")
+     */
+    public function userList() : Response
+    {
+        $userRepo = $this->getDoctrine()->getRepository(User::class);
+        $users = $userRepo->findAll();
+
+        return $this->render("userList.html.twig", ['users' => $users]);
+    }
+
+    /**
+     * @Route("/event/list/{user}", name="event_list")
+     */
+    public function eventList($user)
+    {
+        $eventRepo = $this->getDoctrine()->getRepository(Event::class);
+        $events = array(); // TODO lister les events et afficher un liens de création d'event
+
+        return $this->render("eventList.html.twig");
+    }
 
     /**
      * @Route("/newOperation", name="new_operation")
      */
     public function newOperation() : Response
     {
-
         return $this->render("newOperation.html.twig");
     }
 }
