45bddd66ba19524d9be4a35dbec0ce6bc331fe5f
PhRaz <philippe.razavet@gmail.com>
2019-03-08 19:09:28 +0100

    set amount field label as user name from entity data

--- src/Form/ExpenseType.php
+++ src/Form/ExpenseType.php
@@ -4,16 +4,23 @@ namespace App\Form;
 
 use App\Entity\Expense;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class ExpenseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('amount')
-        ;
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            /** @var Expense $expense */
+            $expense = $event->getData();
+            $form = $event->getForm();
+
+            $form->add('amount', IntegerType::class, ['label' => $expense->getUser()->getName()]);
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
