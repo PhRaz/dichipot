b55aba2a26363d65beb326f67cfebf23f789443d
PhRaz <philippe.razavet@gmail.com>
2019-04-05 11:49:47 +0200

    fix admin script

--- bin/cognitoRemoveUser.sh
+++ bin/cognitoRemoveUser.sh
@@ -3,11 +3,11 @@
 USER_POOL_ID="eu-central-1_eX9w26nmT"
 
 RUN=1
-until $(($RUN == 0))
+until (($RUN == 0))
 do
     echo "Listing users"
     USERS=`aws cognito-idp list-users  --user-pool-id ${USER_POOL_ID} | grep Username | awk -F: '{print $2}' | sed -e 's/\"//g' | sed -e 's/,//g'`
-    if $(( "x$USERS" == "x" ))
+    if [ ! -z "$USERS" ]
     then
         for user in $USERS; do
             echo "Deleting user $user"
