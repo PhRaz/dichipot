1cba3c8dedc97047a9b47f9d8d1a9db10dccdec2
PhRaz <philippe.razavet@gmail.com>
2019-04-10 09:14:27 +0200

    fix operation order by date

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -35,25 +35,25 @@ class EventRepository extends ServiceEntityRepository
 
         if ($full) {
             $query
-                ->leftJoin('operations.user', 'operations_authors')
+                ->innerJoin('operations.user', 'operations_authors')
                 ->addSelect('operations_authors')
-                ->leftJoin('operations_authors.userEvents', 'operations_authors_pseudos')// operation author pseudo
+                ->innerJoin('operations_authors.userEvents', 'operations_authors_pseudos')// operation author pseudo
                 ->addSelect('operations_authors_pseudos')
                 ->andWhere('operations_authors_pseudos.event = event')
-                ->leftJoin('operations.expenses', 'operations_expenses')
+                ->innerJoin('operations.expenses', 'operations_expenses')
                 ->addSelect('operations_expenses')
-                ->leftJoin('operations_expenses.user', 'expenses_authors')
+                ->innerJoin('operations_expenses.user', 'expenses_authors')
                 ->addSelect('expenses_authors')
-                ->leftJoin('expenses_authors.userEvents', 'expenses_authors_pseudos')// expense author pseudo
+                ->innerJoin('expenses_authors.userEvents', 'expenses_authors_pseudos')// expense author pseudo
                 ->addSelect('expenses_authors_pseudos')
                 ->andWhere('expenses_authors_pseudos.event = event')
-                ->orderBy('expenses_authors_pseudos.pseudo', 'ASC');
+                ->addOrderBy('operations.date', 'ASC')
+                ->addOrderBy('expenses_authors_pseudos.pseudo', 'ASC');
         }
 
         $result = $query
             ->getQuery()
             ->getOneOrNullResult();
-
         return $result;
     }
 }
