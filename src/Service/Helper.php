4b459748b8c28d58e6d93e4f95e67a85f01cff8f
PhRaz <philippe.razavet@gmail.com>
2019-04-18 14:15:55 +0200

    fix helper

--- src/Service/Helper.php
+++ src/Service/Helper.php
@@ -17,33 +17,33 @@ Class Helper {
         $balance = array();
         foreach ($event->getOperations() as $operation) {
 
-            $id = $operation->getId();
+            $operationId = $operation->getId();
 
             $totalExpense = 0;
             $totalPayment = 0;
             foreach ($operation->getExpenses() as $expense) {
                 $totalExpense += $expense->getExpense();
                 $totalPayment += $expense->getPayment();
-                $userName = $expense->getUser()->getName();
-                $balance[$id][$userName]['expense'] = $expense->getExpense();
-                $balance[$id][$userName]['payment'] = $expense->getPayment();
-                $balance[$id][$userName]['pseudo'] = $expense->getUser()->getUserEvents()[0]->getPseudo();
+                $userId = $expense->getUser()->getId();
+                $balance[$operationId][$userId]['expense'] = $expense->getExpense();
+                $balance[$operationId][$userId]['payment'] = $expense->getPayment();
+                $balance[$operationId][$userId]['pseudo'] = $expense->getUser()->getUserEvents()[0]->getPseudo();
             }
 
-            foreach ($balance[$id] as $userName => $data) {
-                $amountToPay = $totalExpense / $totalPayment * $balance[$id][$userName]['payment'];
-                $balance[$id][$userName] = array_merge($balance[$id][$userName], [
+            foreach ($balance[$operationId] as $userId => $data) {
+                $amountToPay = $totalExpense / $totalPayment * $balance[$operationId][$userId]['payment'];
+                $balance[$operationId][$userId] = array_merge($balance[$operationId][$userId], [
                     'amountToPay' => $amountToPay,
-                    'balance' => $balance[$id][$userName]['expense'] - $amountToPay
+                    'balance' => $balance[$operationId][$userId]['expense'] - $amountToPay
                 ]);
             }
         }
 
         $total = array();
-        foreach ($balance as $id => $balanceData) {
-            foreach ($balanceData as $userName => $userData) {
-                if (!isset($total[$userName])) {
-                    $total[$userName] = [
+        foreach ($balance as $operationId => $balanceData) {
+            foreach ($balanceData as $userId => $userData) {
+                if (!isset($total[$userId])) {
+                    $total[$userId] = [
                         'expense' => 0,
                         'payment' => 0,
                         'amountToPay' => 0,
@@ -51,10 +51,10 @@ Class Helper {
                         'pseudo' => $userData['pseudo']
                     ];
                 }
-                $total[$userName]['expense'] += $userData['expense'];
-                $total[$userName]['payment'] += $userData['payment'];
-                $total[$userName]['amountToPay'] += $userData['amountToPay'];
-                $total[$userName]['balance'] += $userData['balance'];
+                $total[$userId]['expense'] += $userData['expense'];
+                $total[$userId]['payment'] += $userData['payment'];
+                $total[$userId]['amountToPay'] += $userData['amountToPay'];
+                $total[$userId]['balance'] += $userData['balance'];
             }
         }
         return array($balance, $total);
