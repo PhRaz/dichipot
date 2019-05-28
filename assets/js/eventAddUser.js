69530f9ecd2a5c7dd06fcebfb6c14367a187c838
PhRaz <philippe.razavet@gmail.com>
2019-05-28 14:06:46 +0200

    manage max number of participant

--- assets/js/eventAddUser.js
+++ assets/js/eventAddUser.js
@@ -2,38 +2,92 @@ var $ = require('jquery');
 
 module.exports = function () {
 
-    var addUserForm = function ($collectionHolder, $addUserButton) {
+    const $addUserButton = $('<div class="form-group row"><div class="col-12"><button id="add_user" type="button" class="btn btn-block btn-primary shadow-sm"><i class="fas fa-user-plus"></i></button></div></div>');
+
+    /**
+     * Add event handler on remove button.
+     *
+     * @param $form
+     * @param index
+     */
+    const addUserFormDeleteButton = function ($form, index) {
+        var $removeUserButton = $('#remove' + index);
+        $removeUserButton.on('click', function (e) {
+            $form.remove();
+            /*
+             * enable the 'add participant' button
+             */
+            $('#add_user')
+                .addClass('btn-primary shadow-sm')
+                .removeClass('disabled btn-secondary')
+                .unbind('click')
+                .on('click', function (e) {
+                    addParticipant();
+                })
+                .find('> p')
+                .remove();
+        });
+    };
+
+    /**
+     * Display a form to add a new participant
+     *
+     * @param $collectionHolder
+     * @param $addUserButton
+     */
+    const addUserForm = function ($collectionHolder, $addUserButton) {
         let prototype = $collectionHolder.data('prototype');
         let index = $collectionHolder.data('index');
         let newForm = prototype;
-
         newForm = newForm.replace(/__name__/g, index);
         $collectionHolder.data('index', index + 1);
-
         $newForm = $(newForm);
         $addUserButton.before($newForm);
         addUserFormDeleteButton($newForm, index);
     };
 
-    var addUserFormDeleteButton = function ($form, index) {
-        var $removeUserButton = $('#remove' + index);
-        $removeUserButton.on('click', function (e) {
-            $form.remove();
-        });
+    /**
+     * Check if max nb user is reached and eventually disable the button.
+     */
+    const checkMaxNbUser = function () {
+        /*
+         * check user limit on number of participant
+         */
+        let nbParticipant = $('.user').length;
+        if (nbParticipant >= maxNbParticipant) {
+            /*
+             * disable the 'add participant' button
+             */
+            $('#add_user')
+                .unbind('click')
+                .removeClass('btn-primary shadow-sm')
+                .addClass('disabled btn-secondary')
+                .append('<p>Vous avez atteint le nombre maximum de participants pour un événement.</p>')
+        }
     };
 
-    let $collectionHolder;
-    let $addUserButton = $('<div class="form-group row"><div class="col-12"><button type="button" class="btn btn-block btn-primary shadow-sm"><i class="fas fa-user-plus"></i></button></div></div>');
+    /**
+     * Add form and manage max number of participants.
+     */
+    const addParticipant = function () {
+        addUserForm($collectionHolder, $addUserButton);
+        checkMaxNbUser();
+    };
 
-    $collectionHolder = $('.userEvents');
+    let maxNbParticipant = 5;
+    let $collectionHolder = $('.userEvents');
+
     $collectionHolder.append($addUserButton);
     $collectionHolder.data('index', $collectionHolder.find('.user').length);
 
-    $addUserButton.on('click', function (e) {
-        addUserForm($collectionHolder, $addUserButton);
+    $('#add_user').on('click', function (e) {
+        console.log("click ajout participant");
+        addParticipant();
     });
 
     $('.user').each(function () {
         addUserFormDeleteButton($(this));
-    })
+    });
+
+    checkMaxNbUser();
 };
