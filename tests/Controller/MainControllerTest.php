c31407d1079dfe1d9bed89933dd4ca04c7769628
PhRaz <philippe.razavet@gmail.com>
2019-07-09 18:14:09 +0200

    update test case for en home page

--- tests/Controller/MainControllerTest.php
+++ tests/Controller/MainControllerTest.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class MainControllerTest extends WebTestCase
 {
-    public function testHomePage()
+    public function testHomePageFr()
     {
         $client = static::createClient();
 
@@ -31,7 +31,21 @@ class MainControllerTest extends WebTestCase
         $this->assertCount(5, $crawler->filter('h1'));
 
         $this->assertSelectorTextContains('p.lead.text-center', 'Une solution simple pour partager les dépenses.');
+    }
 
-        $this->assertEquals(5, $crawler->filter('h1')->count());
+    public function testHomePageEn()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', 'http://localhost/en/home');
+
+        $this->assertEquals(
+            200,
+            $client->getResponse()->getStatusCode()
+        );
+
+        $this->assertCount(5, $crawler->filter('h1'));
+
+        $this->assertSelectorTextContains('p.lead.text-center', 'A simple solution to share expenses');
     }
 }
